b6c6c14ecae1b9bcec06fff7305d7319bfcd1b4a
Sindy Stefany <sindystef@gmail.com>
2021-08-31 03:21:26 +0700

    commit

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,6 +4,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import CardContent from '@material-ui/core/CardContent';
 import Img from './Img';
 import Button from '@material-ui/core/Button';
+import Drawer from '@material-ui/core/Drawer';
+import { useState } from 'react';
+import styles from '../styles/Card.module.css'
 
 interface CardProps {
     index: number
@@ -15,30 +18,55 @@ const useStyles = makeStyles({
         marginTop: '7px',
         marginBottom: '7px'
     },
-    button: {
-        backgroundColor: 'rgb(3, 172, 14)',
-        fontSize: 12,
-        fontWeight: 800,
+    buttonOutline: {
+        borderColor: 'rgb(3, 172, 14)',
+        marginRight: '.5em',
+        color: 'rgb(3, 172, 14)'
+    },
+    buttonContained: {
         color: 'white',
-        textTransform: 'capitalize',
+        backgroundColor: 'rgb(3, 172, 14)',
         '&:hover': {
             backgroundColor: 'rgb(3, 172, 14)'
         }
+    },
+    button: {
+        fontSize: 12,
+        fontWeight: 800,
+        textTransform: 'capitalize'
     }
 });
 
 const CardComponent: React.FC<CardProps> = ({ index }) => {
-    const classes = useStyles();
+    const classes = useStyles()
+    const [toggleDrawerOpened, setToggleDrawerOpened] = useState<boolean>(false)
     return (
-        <Card className={classes.root}>
-            <CardContent className={`d-flex direction-column align-center ${index % 4 === 1 ? 'grass' : index % 3 === 1 ? 'fire' : index % 5 === 1 ? 'water' : index % 6 === 2 ? 'bug' : index % 2 === 0 ? 'poison' : 'electric'}`}>
-                <Img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png' alt='trial' />
-                <h3>ivysaur</h3>
-                <Button size="small" variant="contained" className={classes.button}>
-                    Preview
-                </Button>
-            </CardContent>
-        </Card>
+        <React.Fragment>
+            <Card className={classes.root}>
+                <CardContent className={`d-flex direction-column align-center ${index % 4 === 1 ? 'grass' : index % 3 === 1 ? 'fire' : index % 5 === 1 ? 'water' : index % 6 === 2 ? 'bug' : index % 2 === 0 ? 'poison' : 'electric'}`}>
+                    <Img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg' alt='trial' />
+                    <h3>ivysaur</h3>
+                    <Button onClick={() => setToggleDrawerOpened(true)} size="small" variant="contained" className={`${classes.button} ${classes.buttonContained}`}>
+                        Preview
+                    </Button>
+                </CardContent>
+            </Card>
+            <Drawer anchor='bottom' open={toggleDrawerOpened} onClose={() => setToggleDrawerOpened(false)}>
+                <div className={styles?.drawerImg}>
+                    <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg' alt='trial' className={toggleDrawerOpened ? styles?.imgMove : ''} />
+                    <div className={styles?.shadow}></div>
+                </div>
+                <h3 className={styles?.drawerText}>ivysaur</h3>
+                <div className={styles?.drawerButton}>
+                    <Button onClick={() => setToggleDrawerOpened(false)} size="small" variant="outlined" className={`${classes.button} ${classes.buttonOutline}`}>
+                        Batal
+                    </Button>
+                    <Button size="small" variant="contained" className={`${classes.button} ${classes.buttonContained}`}>
+                        Lihat Selengkapnya
+                    </Button>
+                </div>
+            </Drawer>
+        </React.Fragment>
     )
 }
 
